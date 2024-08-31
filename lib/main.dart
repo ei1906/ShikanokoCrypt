3013eeb1344183ec444b3654ec1dbea7106e4ba5
ei1906 <whitegorilla1906@gmail.com>
2024-08-31 12:56:11 +0900

    [add] 入力フォームを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   // This widget is the root of your application.
   @override
@@ -13,37 +14,38 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'しかのこ暗号',
       theme: ThemeData.light(),
-      home: const HomeScreen(),
+      home: getHomeScreen(),
     );
   }
-}
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
+  Widget getHomeScreen() {
+    return Scaffold(
         body: Center(
-      child: Column(children: [
-        TitleLogo(),
-        TextField(
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            hintText: 'Enter a search term',
-          ),
-        ),
-      ]),
+      child: SizedBox(
+        width: 700,
+        child: getAppContents(),
+      ),
     ));
   }
-}
 
-class TitleLogo extends StatelessWidget {
-  const TitleLogo({super.key});
+  Widget getAppContents() {
+    return Column(children: [
+      getTitleLogo(),
+      getInputForm('Input'),
+    ]);
+  }
 
-  @override
-  Widget build(BuildContext context) {
+  Widget getTitleLogo() {
     // 一時的にテキストを配置
     return const Text('しかのこ暗号');
+  }
+
+  Widget getInputForm(String hint) {
+    return const TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        hintText: 'Input Cipher or Crypt',
+      ),
+    );
   }
 }
